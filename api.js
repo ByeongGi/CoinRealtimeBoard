f2df3c24f20069cc48997f67ef3050d8dcae1355
kira <wwww3426@naver.com>
2017-08-06 08:15:23 +0900

    UI 변경

--- api.js
+++ api.js
@@ -5,10 +5,11 @@ $(document).ready(()=>{
 });
 
 
+
 function apiCaller() {
 
     var myHeaders = new Headers();
-
+    
     var myInit = {
         method: 'GET',
         headers: myHeaders,
@@ -18,21 +19,16 @@ function apiCaller() {
 
     var myRequest = new Request('https://api.coinone.co.kr/ticker/?currency=all%27&format=json', myInit);
 
-    fetch(myRequest).then(function (response) {
+    fetch(myRequest).then((response)=>{
 
         return response.json();
     }).then((json) => {
-        
-        $('#now').text(new Date().toLocaleString());
-        $('#timestamp').text(json['timestamp']);
-        $('#high').text(json['btc']['high']);
-        $('#low').text(json['btc']['low']);
-        $('#last').text(json['btc']['last']);
-        $('#first').text(json['btc']['first']);
-        $('#volume').text(json['btc']['volume']);
 
-
-
+        initBtc(json);
+        initEtc(json);
+        initEth(json);
+        initXrp(json);
+       
         console.log("-----------------------------------------");
         console.log(json.timestamp);
         // console.log(json['btc']);
@@ -46,4 +42,47 @@ function apiCaller() {
         console.log("-----------------------------------------");
 
     });
+}
+
+
+function initBtc(json){
+    $('#btc-now').text(new Date().toLocaleString());
+    $('#btc-timestamp').text(json['timestamp']);
+    $('#btc-high').text(json['btc']['high']);
+    $('#btc-low').text(json['btc']['low']);
+    $('#btc-last').text(json['btc']['last']);
+    $('#btc-first').text(json['btc']['first']);
+    $('#btc-volume').text(json['btc']['volume']);
+}
+
+
+function initEtc(json){
+    $('#etc-now').text(new Date().toLocaleString());
+    $('#etc-timestamp').text(json['timestamp']);
+    $('#etc-high').text(json['etc']['high']);
+    $('#etc-low').text(json['etc']['low']);
+    $('#etc-last').text(json['etc']['last']);
+    $('#etc-first').text(json['etc']['first']);
+    $('#etc-volume').text(json['etc']['volume']);
+}
+
+function initEth(json){
+    $('#eth-now').text(new Date().toLocaleString());
+    $('#eth-timestamp').text(json['timestamp']);
+    $('#eth-high').text(json['eth']['high']);
+    $('#eth-low').text(json['eth']['low']);
+    $('#eth-last').text(json['eth']['last']);
+    $('#eth-first').text(json['eth']['first']);
+    $('#eth-volume').text(json['eth']['volume']);
+}
+
+
+function initXrp(json){
+    $('#xrp-now').text(new Date().toLocaleString());
+    $('#xrp-timestamp').text(json['timestamp']);
+    $('#xrp-high').text(json['xrp']['high']);
+    $('#xrp-low').text(json['xrp']['low']);
+    $('#xrp-last').text(json['xrp']['last']);
+    $('#xrp-first').text(json['xrp']['first']);
+    $('#xrp-volume').text(json['xrp']['volume']);
 }
